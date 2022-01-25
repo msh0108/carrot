3f155197569bbf8f1018031c3e8cbc9c0cc22c30
spig0126 <spig0126@gmail.com>
2022-01-25 20:13:06 +0900

    Feat: Create TownPage with TownInfo and TownStore

---
+++ src/components/Town/TownPage.js
@@ -0,0 +1,13 @@
+import TownInfo from "./TownInfo";
+import TownStore from "./TownStore";
+
+function TownPage() {
+	return (
+		<>
+			<TownInfo />
+			<TownStore />
+		</>
+	);
+}
+
+export default TownPage;
