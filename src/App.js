f2522db9e3846292f108b341acd87f8407286335
spig0126 <spig0126@gmail.com>
2022-02-03 17:48:54 +0900

    Feat: add routing features

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import SignPage from "./pages/SignPage";
+import SelectRegion from "./components/MyPage/SelectRegion/SelectRegion";
+import { Link } from "react-router-dom";
 
 function App() {
-    return (
+	return (
 		<>
-			<SignPage />
-        </>
-    );
+			<Link to="town">TownPage</Link>
+			<Link to="login">Login</Link>
+		</>
+	);
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import reportWebVitals from "./reportWebVitals";
 
+import "./index.css";
+import App from "./App";
+import TownPage from "./pages/TownPage"
+import LoginPage from "./pages/LoginPage";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App/>
-  </React.StrictMode>,
-  document.getElementById("root")
+	<React.StrictMode>
+		<BrowserRouter>
+			<Routes>
+				<Route path="/" element={<App />} />
+				<Route path="/town" element={<TownPage />} />
+				<Route path="/login" element={<LoginPage />} />
+			</Routes>
+		</BrowserRouter>
+	</React.StrictMode>,
+	document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
