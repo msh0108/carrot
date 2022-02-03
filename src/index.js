2fd6e2fcb70ce45ad768be177d1258394d55bd1a
kmijin <mj010516@naver.com>
2022-02-03 22:53:06 +0900

    push for merge

--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import FAQPage from :"./pages/
 
 
 ReactDOM.render(
   <React.StrictMode>
     <App/>
+    <Route path="" elements{<} 
   </React.StrictMode>,
   document.getElementById("root")
 );
