680f0a481aa1a7c362d0c6390faecea940d4f97d
SeungHyeok Myeong <audtmdgur@naver.com>
2022-02-03 19:30:32 +0900

    push for merge

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import SignPage from "./pages/SignPage";
+import Mypost from "./components/page/Mypost";
+import Top from "./components/page/Top";
+import Bottom from "./components/page/Bottom";
 
 function App() {
     return (
 		<>
-			<SignPage />
+            <Top />
+			<Mypost />
+            <Bottom />
         </>
     );
 }
