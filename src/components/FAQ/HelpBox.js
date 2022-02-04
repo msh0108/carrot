a57e5965183bd33c4dc4a3c9431114aa3eae7208
SeungHyeok Myeong <audtmdgur@naver.com>
2022-02-04 22:40:03 +0900

    merge

---
+++ src/components/FAQ/HelpBox.js
@@ -0,0 +1,32 @@
+import styled from "styled-components";
+
+function HelpBox() {
+  return (
+    <HelpBoxWrap>
+      <p>그래도 해결이 안되시나요?</p>
+
+      <span as="a" href="#">
+        고객센터 문의하기
+      </span>
+    </HelpBoxWrap>
+  );
+}
+
+export default HelpBox;
+
+const HelpBoxWrap = styled.div`
+  margin: 0 auto;
+  width: 100%;
+  height: 15vh;
+  background-color: #eee;
+  text-align: center;
+  p {
+    font-size: 1.8vh;
+    font-weight: bold;
+    line-height: 9vh;
+  }
+  span {
+    color: #f04124;
+    font-weight: bold;
+  }
+`;
